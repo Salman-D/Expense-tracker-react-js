a7e7027bede5bc684c1247ddca66db2af3a6282d
Salman-D <mohammadsalman3110@gmail.com>
2022-06-01 15:29:04 +0500

    Complete project

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <>
+
+      <marquee behavior="scroll" direction="right">
+        <h2>
+          Expense Tracker | Muhammad Salman
+        </h2>
+      </marquee>
+    </>
+  )
+}
